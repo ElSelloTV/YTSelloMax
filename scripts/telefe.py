7f72443d8d69c751218e9d2e2934fcb52a1afa74
sellotv <108602758+ElSelloTV@users.noreply.github.com>
2023-12-24 19:54:32 -0300

    Update telefe.py

--- scripts/telefe.py
+++ scripts/telefe.py
@@ -1,45 +1,39 @@
 #! /usr/bin/python3
 
-import requests
 import os
 import sys
+import requests
 
 windows = False
 if 'win' in sys.platform:
     windows = True
 
 def grab(url):
-    response = s.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/ElSelloTV/YTSelloMax/main/assets/info.m3u8')
-                return
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/ElSelloTV/YTSelloMax/main/assets/info.m3u8')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
-    hd = streams[-1].strip()
-    st = hd.find('http')
-    print(hd[st:].strip())
+    response = requests.get(url).text
+    m3u8_index = response.find('.m3u8')
+    
+    if m3u8_index != -1:
+        start_index = max(response.rfind('https://', 0, m3u8_index), response.rfind('http://', 0, m3u8_index))
+        end_index = response.find('.m3u8', m3u8_index) + 5
+        stream_url = response[start_index:end_index]
+        print(stream_url)
+    else:
+        print(f"No se encontró enlace .m3u8 para {url}")
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
-s = requests.Session()
 
-# Cambié el archivo de entrada a tu URL específica
-grab('https://telefe.com/Api/Videos/GetSourceUrl/694564/0/HLS?.m3u8')
+with open('../telefe.txt') as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        if not line.startswith('https:'):
+            line = line.split('|')
+            ch_name = line[0].strip()
+            grp_title = line[1].strip().title()
+            tvg_logo = line[2].strip()
+            tvg_id = line[3].strip()
+        else:
+            grab(line)
