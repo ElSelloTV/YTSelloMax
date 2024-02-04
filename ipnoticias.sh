bd0400fce3b5fedad25df9ce954639349a4dbfb4
sellotv <108602758+ElSelloTV@users.noreply.github.com>
2024-02-04 19:56:56 -0300

    Update ipnoticias.sh

--- ipnoticias.sh
+++ ipnoticias.sh
@@ -12,11 +12,8 @@ user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 # Datos a enviar en la solicitud POST
 post_data="submit=ACTIVAR+AHORA%21"
 
-# Archivo de registro
-log_file="canales.log"
-
 # Realiza una solicitud POST utilizando wget con el User-Agent de Chrome y datos en el cuerpo de la solicitud
-wget --no-check-certificate --user-agent="$user_agent" --post-data="$post_data" "$url" -O response.html >> "$log_file" 2>&1
+wget --no-check-certificate --user-agent="$user_agent" --post-data="$post_data" "$url" -O response.html
 
 # Verifica si la respuesta contiene información relevante
 if grep -q "ACTIVADO" response.html; then
