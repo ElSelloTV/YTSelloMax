55392a8879540e0ad58ff994d7f9f3e3d886ee25
sellotv <108602758+ElSelloTV@users.noreply.github.com>
2024-02-09 18:02:30 -0300

    Update ipnoticias.py

--- scripts/ipnoticias.py
+++ scripts/ipnoticias.py
@@ -1,62 +1,58 @@
 #! /usr/bin/python3
 
-
 import requests
 import os
-import sys
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
+client_id = '4p7co79ke3c09hydlf071ouvy8coa1'  # Tu Client-ID de Twitch
 
-def grab(url):
-    response = s.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/ElSelloTV/YTSelloMax/main/assets/info.m3u8')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/ElSelloTV/YTSelloMax/main/assets/info.m3u8')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
-    hd = streams[-1].strip()
-    st = hd.find('http')
-    print(hd[st:].strip())
-    #print(f"{link[start : end]}")
+def grab_twitch(channel_name):
+    headers = {
+        'Client-ID': client_id,
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'operationName': "PlaybackAccessToken_Template",
+        'query': """
+        query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {
+          playbackAccessToken(login: $login, isLive: $isLive, vodID: $vodID, isVod: $isVod, playerType: $playerType) {
+            value
+            signature
+            __typename
+          }
+        }""",
+        'variables': {
+            'isLive': True,
+            'login': channel_name,
+            'isVod': False,
+            'vodID': '',
+            'playerType': 'channel_home_live',
+        }
+    }
+    response = requests.post('https://gql.twitch.tv/gql', headers=headers, json=data)
+    if response.status_code == 200:
+        json_response = response.json()
+        token = json_response['data']['playbackAccessToken']['value']
+        sig = json_response['data']['playbackAccessToken']['signature']
+        m3u8_url = f"https://usher.ttvnw.net/api/channel/hls/{channel_name}.m3u8?client_id={client_id}&token={token}&sig={sig}&allow_audio_only=true&allow_source=true&type=any"
+        print(m3u8_url)
+    else:
+        print("Error fetching Twitch M3U8 link")
 
-print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
-s = requests.Session()
-with open('../ipnoticias.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-        else:
-            grab(line)
+def main():
+    print('#EXTM3U')
+    print('#EXT-X-VERSION:3')
+    print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+    with open('../ipnoticias.txt') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('~~'):
+                continue
+            elif 'twitch.tv' in line:
+                channel_name = line.split('/')[-1]  # Asume que la URL es del formato twitch.tv/nombre_del_canal
+                grab_twitch(channel_name)
+
+if __name__ == "__main__":
+    main()
 
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
-    os.system('rm watch*')
